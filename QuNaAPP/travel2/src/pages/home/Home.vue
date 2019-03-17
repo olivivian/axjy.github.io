2b79ba9fac1d6822698dc3d318e7fe102d432931
XJY <1924485758@qq.com>
2019-03-17 15:38:43 +0800

    project init

---
+++ QuNaAPP/travel2/src/pages/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>home</div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
