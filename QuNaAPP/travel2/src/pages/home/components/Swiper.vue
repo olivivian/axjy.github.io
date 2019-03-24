236c778519387c5449df053f027f8b76d6525d2f
XJY <1924485758@qq.com>
2019-03-24 15:15:58 +0800

    修改

--- QuNaAPP/travel2/src/pages/home/components/Swiper.vue
+++ QuNaAPP/travel2/src/pages/home/components/Swiper.vue
@@ -5,7 +5,6 @@
       <swiper-slide v-for="item of swiperList" :key="item.id">
         <img class="swiper-img" :src='item.imgUrl'/>
       </swiper-slide>
-
       <div class="swiper-pagination"  slot="pagination"></div>
     </swiper>
   </div>
@@ -36,10 +35,12 @@ export default {
 </script>
 
 <!--
-padding-bottom 30.75%
-31.25%是图片的宽高比例，
-保证宽高比例始终保持为30.75
+padding-bottom 26.66%
+26.66%是图片的宽高比例，
+保证宽高比例始终保持为26.66
 实现图片宽高比例自适应
+
+计算公式  高(200)/宽(750)*100
 
 为了脱离scoped的限制，对页面类做一个穿透
  .wrapper >>>.swiper-pagination-bullet-active
@@ -51,7 +52,7 @@ padding-bottom 30.75%
     width: 100%
     height: 0
     overflow: hidden
-    padding-bottom:30.75%
+    padding-bottom:26.66%
     background: #eee
     .swiper-img
       width: 100%
