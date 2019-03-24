2284897544d1738012c8552fea2ce618a8613820
XJY <1924485758@qq.com>
2019-03-24 15:01:51 +0800

    change

---
+++ QuNaAPP/travel2/src/pages/home/components/Swiper.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src='item.imgUrl'/>
+      </swiper-slide>
+
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption:{
+        pagination:'.swiper-pagination',
+        loop:true
+      },
+      swiperList:[
+          {
+          id:'0001',
+            imgUrl:require('../../../assets/img/banner1.jpg')
+        },
+        {
+          id:'0002',
+          imgUrl:require('../../../assets/img/banner2.jpg')
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<!--
+padding-bottom 30.75%
+31.25%是图片的宽高比例，
+保证宽高比例始终保持为30.75
+实现图片宽高比例自适应
+
+为了脱离scoped的限制，对页面类做一个穿透
+ .wrapper >>>.swiper-pagination-bullet-active
+-->
+<style lang="stylus" scoped>
+  .wrapper >>>.swiper-pagination-bullet-active
+    background #fff
+  .wrapper
+    width: 100%
+    height: 0
+    overflow: hidden
+    padding-bottom:30.75%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
